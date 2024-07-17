e1b210cf9e4dbcc170f62458a1b23311e796d3a5
skdarapu <darapushivakumar@gmail.com>
2024-07-17 22:37:02 +0530

    Create copy-files.sh

---
+++ copy-files.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+aws configure set aws_access_key_id AWS_ACCESS_KEY_ID
+aws configure set aws_secret_access_key AWS_SECRET_ACCESS_KEY
+aws configure set region AWS_DEFAULT_REGION
+file_name=$(ls -ltc FILE_PATH | head -n 2 | tail -n 1 | awk '{print $NF}')
+echo '------------------ Copying Files To Bucket -----------------------------'
+aws s3 cp FILE_PATH/$file_name s3://BUCKET_NAME/
+ls -al
+echo '------------ DONE -------------------------------------------------------'
